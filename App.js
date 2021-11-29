8b09756cf41eea57e8537d8e48e50e9d520dc54a
Plasma Eios <versoz1@stolaf.edu>
2021-11-29 15:39:56 -0600

    Fixes merge conflicts

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import Home from './Home.js';
-<<<<<<< HEAD
-import CafComments from './CafComments'
-=======
 // import CafComments from './CafCommentsClass'
 import CafComments from './CafComments.js';
->>>>>>> areeba
 
 export default class MyApp extends Component {
   render() {
     return (
-<<<<<<< HEAD
-=======
       // <Home/>
->>>>>>> areeba
       <CafComments/>
     )
   }
