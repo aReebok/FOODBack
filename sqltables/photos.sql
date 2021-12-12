78f69f285da391944a1764adb94b720ada98a0f2
Victoria <nguyen51@stolaf.edu>
2021-12-12 01:20:43 -0600

    modified photos.sql

--- sqltables/photos.sql
+++ sqltables/photos.sql
@@ -2,11 +2,12 @@ DROP TABLE IF EXISTS photos;
 
 CREATE TABLE photos(
     pid SMALLSERIAL PRIMARY KEY,
-    uid references users(uid),
+    uid int references users(uid),
+    date    DATE DEFAULT CURRENT_DATE,
     url VARCHAR NOT NULL,
 
     unique(pid, url)
-)
+);
 
 INSERT INTO photos (uid, url) VALUES (1, 'https://i.imgur.com/4021Epi.jpg');
 INSERT INTO photos (uid, url) VALUES (1, 'https://i.imgur.com/hcoLG5k.jpg');
