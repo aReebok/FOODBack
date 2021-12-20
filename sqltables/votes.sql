1fa0246139158b5eea1607d56db91874773285ae
Victoria Nguyen <nguyen51@stolaf.edu>
2021-12-20 05:00:47 +0000

    added votes.sql, sql table for votes

---
+++ sqltables/votes.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS votes;
+
+CREATE TABLE votes(
+    vid             SMALLSERIAL PRIMARY KEY,
+    cid             int references comment(cid)
+    uid             int references users(uid),
+
+    response        VARCHAR NOT NULL,
+    unique(cid, uid)
+);
